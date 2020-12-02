2b81aded6ca609b760113f7ff6e402579d30d9a8
LAMAH Michel Marie <lamahmichelmarie@gmail.com>
2020-12-02 04:36:01 +0100

    Cette partie consistait à l'utilisation d'une base de données

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿namespace RazorPagesMovie.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using RazorPagesMovie.Data;
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Defines the <see cref="SeedData" />.
+    /// </summary>
+    public class SeedData
+    {
+        /// <summary>
+        /// The Initialize.
+        /// </summary>
+        /// <param name="serviceProvider">The serviceProvider<see cref="IServiceProvider"/>.</param>
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.Movie.AddRange(
+                     new Movie
+                     {
+                         Title = "When Harry Met Sally",
+                         ReleasedDate = DateTime.Parse("1989-2-12"),
+                         Genre = "Romantic Comedy",
+                         Price = 7.99M
+                     },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleasedDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleasedDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleasedDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
