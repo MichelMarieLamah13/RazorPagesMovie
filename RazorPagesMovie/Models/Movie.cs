c4f21c5ff6e4f07402788bc43b5b662de35dfa14
LAMAH Michel Marie <lamahmichelmarie@gmail.com>
2020-12-03 00:52:09 +0100

    Dans cette partie, nous allons apprendre à ajouter un nouveau champ à notre modèle

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -17,5 +17,6 @@ namespace RazorPagesMovie.Models
         public string Genre { get; set; }
         [Column(TypeName ="Decimal(18,2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
